578d265f5b4cbfd4e76360e8b962a10be8a2c64c
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-07-05 09:27:37 +0200

    Initial commit

---
+++ lagalt-be/src/test/java/com/lagaltcase/lagalt_be/LagaltBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagaltcase.lagalt_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
