4109f16d1768b7370db6a3800ad16a5462bbe7ad
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-07-09 09:06:44 +0200

    Added hidden, description, skills fields to user

--- lagalt-be/src/main/java/com/lagaltcase/lagalt_be/user/User.java
+++ lagalt-be/src/main/java/com/lagaltcase/lagalt_be/user/User.java
@@ -37,6 +37,17 @@ public class User {
     @Column
     private String password;
 
+    @Column
+    private String description;
+
+    @Column
+    private boolean hidden;
+
+    @ElementCollection //To store a collection of strings in database (OneToMany is only for objects that are entities/tables)
+    @CollectionTable(name = "user_skills", joinColumns = @JoinColumn(name = "user_id")) //Sets FK and names table and column
+    @Column
+    private List<String> skills;
+
     //Projects this user owns. A user can own many projects. A project can have only one owner
     @OneToMany(mappedBy = "user")
     @JsonIgnoreProperties("user")
