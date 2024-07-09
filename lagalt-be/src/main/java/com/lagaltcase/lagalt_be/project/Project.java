ede8b59c96f846070990bf17cc4302cfe48ff731
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-07-09 09:31:14 +0200

    Added user portfolio ie contributed projects

--- lagalt-be/src/main/java/com/lagaltcase/lagalt_be/project/Project.java
+++ lagalt-be/src/main/java/com/lagaltcase/lagalt_be/project/Project.java
@@ -89,6 +89,15 @@ public class Project {
     @JsonIgnoreProperties("visitedProjects")
     private List<User> visitors = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(
+            name = "user_contributed_projects",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    @JsonIgnoreProperties("contributedProjects")
+    private List<User> contributors = new ArrayList<>();
+
 
     public Project(String title, String description, String category, String websiteUrl, User user) {
         this.title = title;
