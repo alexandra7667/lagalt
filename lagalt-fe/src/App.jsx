a9c34b94c3234377962528ae890cc6750eba60a8
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-07-11 16:01:41 +0200

    Added Material UI

--- lagalt-fe/src/App.jsx
+++ lagalt-fe/src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import Main from './Main/Main'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline'; //Resets browser css
 
 function App() {
 
   return (
     <>
-      <Main/ >
+      <CssBaseline enableColorScheme /> {/* enableColorScheme sets the theme to be the user's system theme */}
+      <Button variant="contained" sx={{ mb: 4 }}>MUI Button</Button>
+      <Main />
     </>
   )
 }
