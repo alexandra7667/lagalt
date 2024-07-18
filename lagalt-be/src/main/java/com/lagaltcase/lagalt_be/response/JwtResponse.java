82c270cc2818db0607565e57d20e90ea80e63861
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-07-18 17:12:18 +0200

    Log in returns all user info

--- lagalt-be/src/main/java/com/lagaltcase/lagalt_be/response/JwtResponse.java
+++ lagalt-be/src/main/java/com/lagaltcase/lagalt_be/response/JwtResponse.java
@@ -3,6 +3,8 @@ package com.lagaltcase.lagalt_be.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 
 @Getter
 @Setter
@@ -12,12 +14,18 @@ public class JwtResponse {
     private int id;
     private String email;
     private String username;
+    private String description;
+    private boolean hidden;
+    private List<String> skills;
 
-    public JwtResponse(String token, int id, String email, String username) {
+    public JwtResponse(String token, int id, String email, String username, String description, boolean hidden, List<String> skills) {
         this.token = token;
         this.type  = "Bearer";
         this.id = id;
         this.email = email;
         this.username = username;
+        this.description = description;
+        this.hidden = hidden;
+        this.skills = skills;
     }
 }
--- lagalt-be/src/main/java/com/lagaltcase/lagalt_be/user/UserController.java
+++ lagalt-be/src/main/java/com/lagaltcase/lagalt_be/user/UserController.java
@@ -2,18 +2,21 @@ package com.lagaltcase.lagalt_be.user;
 
 
 import com.lagaltcase.lagalt_be.dto.UserDTO;
-import com.lagaltcase.lagalt_be.project.Project;
 import com.lagaltcase.lagalt_be.project.ProjectRepository;
 import com.lagaltcase.lagalt_be.request.UserRequest;
 import com.lagaltcase.lagalt_be.response.ErrorResponse;
 import com.lagaltcase.lagalt_be.response.UserResponse;
+import com.lagaltcase.lagalt_be.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*") //Ändra
 @RestController
@@ -30,7 +33,6 @@ public class UserController {
     //If admin wants to create a user. Not used for regular sign up - See AuthController
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody User user) { //Matches User json object props to User model fields. Does not use constructor for this
-
         //Add Role manually because the role name needs to be retrieved from RoleRepository
         List<Role> roles = new ArrayList<>();
         Role role = roleRepository.findByName(user.getRoles().get(0).getName())
@@ -74,19 +76,45 @@ public class UserController {
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<?> getUserById(@PathVariable int userId) {
-        User user = this.userRepository.findById(userId).orElse(null);
+//    @GetMapping("/{userId}")
+//    public ResponseEntity<?> getUserById(@PathVariable int userId) {
+//        User user = this.userRepository.findById(userId).orElse(null);
+//
+//        if (user == null) {
+//            ErrorResponse errorResponse = new ErrorResponse();
+//            errorResponse.set("No user with that id found.");
+//
+//            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+//        }
+//
+//        UserDTO userDTO = new UserDTO(user);
+//
+//        UserResponse userResponse = new UserResponse();
+//        userResponse.set(userDTO);
+//
+//        return ResponseEntity.ok(userResponse);
+//    }
+
+    @GetMapping //token is sent in header
+    public ResponseEntity<?> getUserByToken() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        UserDetailsImpl userDetailsImpl = (UserDetailsImpl) authentication.getPrincipal();
+
+        User user = userRepository.findByUsername(userDetailsImpl.getUsername()).orElse(null);
 
         if (user == null) {
             ErrorResponse errorResponse = new ErrorResponse();
-            errorResponse.set("No user with that id found.");
+            errorResponse.set("No user with that token found.");
 
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
 
         UserDTO userDTO = new UserDTO(user);
-
         UserResponse userResponse = new UserResponse();
         userResponse.set(userDTO);
 
