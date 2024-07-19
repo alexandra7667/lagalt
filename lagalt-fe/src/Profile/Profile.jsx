7fe6fa2b374bcccfd7ecae7826019d8f588cad49
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-07-19 08:12:50 +0200

    Added user description

--- lagalt-fe/src/Profile/Profile.jsx
+++ lagalt-fe/src/Profile/Profile.jsx
@@ -96,6 +96,7 @@ function Profile() {
                                 id="description"
                                 name="description"
                                 label="Description"
+                                value={user.description}
                                 onChange={handleChange}>
                             </TextField>
 
